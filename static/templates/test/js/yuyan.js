fce10ac2b78834955e7548481c316f7dc1742f1b
chenzx <cc537400>
2021-08-23 17:51:41 +0800

    系统改动

---
+++ static/templates/test/js/yuyan.js
@@ -0,0 +1,239 @@
+var yuyan = {
+  en: {
+    general: {
+        title: "BITO",
+        bitopro: "BitoPro",
+        bitoproIntroduce: "Fiat-Crypto Exchange",
+        bitoex: "BitoEX",
+        bitoexIntroduce: "E-Wallet",
+        slogan: "Providing Comprehensive <br /> Crypto Exchange Services",
+        products: "Products",
+        userFeedback: "User feedback",
+        faq: "FAQ",
+        chooseYourProduct: "Choose your product",
+        partners: "Partners"
+    },
+    actionButton: {
+        tryNow: "Try now",
+        startToTrade: "Trade Now"
+    },
+    sectionTitle: {
+        products: "Products",
+        userFeedback: "User feedback",
+        faq: "FAQ",
+        chooseYourProduct: "Choose your product",
+        partners: "Partners"
+    },
+    introduce: {
+        bitopro: {
+            text_1: "Highest trading volume of 3 billion TWD in a day",
+            text_2: "Providing 20 types of cryptocurrency",
+            text_3: "Top 10 Fintech Startups in Taiwan",
+            text_4: " World's first patented product “TTCheck”",
+            card: {
+                text_1: "For professionals ",
+                text_2: "Familiar with stock market trading",
+                text_3: "Trading multiple cryptocurrencies"
+            }
+        },
+        bitoex: {
+            text_1: "No.1 market share wallet service in Taiwan",
+            text_2: "Top 5 in Bitcoin Wallet's Trading Volume",
+            text_3: "Top 10 APEC Payment Solution Companies 2017",
+            text_4: "Provides services for over 3,000 CVS",
+            card: {
+                text_1: "For beginners",
+                text_2: "Conveniently purchase",
+                text_3: "High volume and quick transactions "
+            }
+        }
+    },
+    userFeedback: {
+        user1: {
+            say: "I purchased BTC at a convenient store. It was easy to register and operate through the website's interface. Now I'm learning how to purchase cryptocurency through bank wire."
+        },
+        user2: {
+            say: "I work in Taiwan so I bought the cryptocurrency with TWD. Once I got the cryptocurrency, I can easily transfer these assets to BitoEX and manage it."
+        },
+        user3: {
+            say: "I'm have invested in many things and crypocurrency has become an important investment of mine. I have been trading crypocurrency on BitoPro for a long time, its encryption algorithm is worthy of my trust."
+        }
+    },
+    faq: {
+        question1: {
+            title: "Is it legal to buy Bitcoin?",
+            answer: "Yes, it is legal and the central bank defines it as digital merchandise. The application and use of membership will be handled in accordance with the relevant compliance towards the KYC and AML law."
+        },
+        question2: {
+            title: "Why is it so hard to buy one Bitcoin?",
+            answer: "On BitoEX, the minimum purchase amount of Bitcoin is 200 TWD. On BitoPro, the minimum purchase amount of Bitcoin is 0.0025, which is about 725 TWD. In addition, you need to deposit TWD to start trading. If you want to withdrawal TWD after selling cryptocurrencies, the TWD will be transferred to the bank account that you have bound."
+        },
+        question3: {
+            title: "How much are the fees?",
+            answer: "The transaction fees for the two platforms are different. BitoEX fees are included in the real-time quotes. BitoPro have two types of transaction fees: Maker Fees are 0.1% of transaction fees, Taker Fees are 0.2% of transaction fees. "
+        }
+    },
+    footer: {
+        contactUs: "Contact us",
+        press: "Press"
+    }
+  },
+  "zh-TW": {
+    general: {
+        title: "BITO 幣託",
+        bitopro: "BitoPro",
+        bitoproIntroduce: "數位資產交易平台",
+        bitoex: "BitoEX",
+        bitoexIntroduce: "虛擬貨幣錢包買賣平台",
+        slogan: "提供您最完整虛擬通貨服務",
+        products: "產品服務",
+        userFeedback: "用戶回饋",
+        faq: "常見問題",
+        chooseYourProduct: "選擇您的產品",
+        partners: "合作伙伴"
+    },
+    actionButton: {
+        tryNow: "前往交易",
+        startToTrade: "立即體驗"
+    },
+    sectionTitle: {
+        products: "我們的產品",
+        userFeedback: "用戶回饋",
+        faq: "常見問題",
+        chooseYourProduct: "選擇您的產品",
+        partners: "合作伙伴"
+    },
+    introduce: {
+        bitopro: {
+            text_1: "全台最大單日 3 億台幣交易量",
+            text_2: "提供 20 種幣種、39 個交易對",
+            text_3: "榮獲 2019 台灣 TOP 10 金融科技新創事業",
+            text_4: "全球首創 「TTCheck」機制",
+            card: {
+                text_1: "適合進階使用者",
+                text_2: "熟習股市看盤下單者",
+                text_3: "交易多種虛擬貨幣"
+            }
+        },
+        bitoex: {
+            text_1: "比特幣電子錢包台灣佔有率 No.1",
+            text_2: "全球比特幣電子錢包交易量 TOP 5",
+            text_3: "2017 榮獲矽谷 CIO 支付解決方案 TOP 10",
+            text_4: "服務涵蓋全台超商通路超過 3,000 家",
+            card: {
+                text_1: "適合初階使用者",
+                text_2: "追求方便快速購買者",
+                text_3: "追求大額交易及快速成交者"
+            }
+        }
+    },
+    userFeedback: {
+        user1: {
+            say: "我都在便利商店買比特幣，註冊介面跟買賣操作流程很簡單。現在我也開始學習使用銀行轉帳購買虛擬貨幣。"
+        },
+        user2: {
+            say: "I work in Taiwan so I bought the crypto currency with TWD. Once I got the currency, I can easily put in the asset and manage it."
+        },
+        user3: {
+            say: "我平時就有投資的習慣，虛擬貨幣是我很重要投資項目。一直以來我都在幣託進行交易，他的加密機制值得我的信賴。"
+        }
+    },
+    faq: {
+        question1: {
+            title: "購買比特幣合法嗎?",
+            answer: "合法。央行定位為數位虛擬商品，但會員的申請與入會使用會依照實名制與洗錢防制法的相關規定辦理。"
+        },
+        question2: {
+            title: "比特幣購買一顆的門檻好高?",
+            answer: "在 BitoEX，比特幣的最小購買金額是新台幣 200 元；在 BitoPro，比特幣的最小購買量是 0.0025 顆，約新台幣 725 元就能買到比特幣。此外，銀行帳號匯入款項加值後，必須入金才能購買；反之售出要領回新台幣，須要出金，錢才會匯進您綁定的銀行帳號。"
+        },
+        question3: {
+            title: "購買比特幣的手續費是?",
+            answer: "兩個平台的費用標準不一樣。BitoEX 手續費包含在即時報價中；BioPro 則採兩種費率，市價買進及市價賣出依成交金額收0.2%的手續費，反之掛單成交則依成交金額收 0.1％。"
+        }
+    },
+    footer: {
+        contactUs: "聯絡我們",
+        press: "媒體報導"
+    }
+  },
+  "zh-CN": {
+    general: {
+        title: "BITO 币托",
+        bitopro: "BitoPro",
+        bitoproIntroduce: "数位资产交易平台",
+        bitoex: "BitoEX",
+        bitoexIntroduce: "虚拟货币钱包买卖平台",
+        slogan: "提供您最完整虚拟通货服务",
+        products: "产品服务",
+        userFeedback: "用户回馈",
+        faq: "常见问题",
+        chooseYourProduct: "选择您的产品",
+        partners: "合作伙伴"
+    },
+    actionButton: {
+        tryNow: "前往交易",
+        startToTrade: "立即体验"
+    },
+    sectionTitle: {
+        products: "我们的产品",
+        userFeedback: "用户回馈",
+        faq: "常见问题",
+        chooseYourProduct: "选择您的产品",
+        partners: "合作伙伴"
+    },
+    introduce: {
+        bitopro: {
+            text_1: "全台最大单日 3 亿台币交易量",
+            text_2: "提供 20 种币种、39 个交易对",
+            text_3: "荣获 2019 台湾 TOP 10 金融科技新创事业",
+            text_4: "全球首创 “TTCheck”机制",
+            card: {
+                text_1: "适合进阶使用者",
+                text_2: "熟习股市看盘下单者",
+                text_3: "交易多种虚拟货币"
+            }
+        },
+        bitoex: {
+            text_1: "比特币电子钱包台湾占有率 No.1",
+            text_2: "全球比特币电子钱包交易量 TOP 5",
+            text_3: "2017 荣获矽谷 CIO 支付解决方案 TOP 10",
+            text_4: "服务涵盖全台超商通路超过 3,000 家",
+            card: {
+                text_1: "适合初阶使用者",
+                text_2: "追求方便快速购买者",
+                text_3: "追求大额交易及快速成交者"
+            }
+        }
+    },
+    userFeedback: {
+        user1: {
+            say: "我都在便利商店买比特币，注册介面跟买卖操作流程很简单。现在我也开始学习使用银行转帐购买虚拟货币。"
+        },
+        user2: {
+            say: "I work in Taiwan so I bought the crypto currency with TWD. Once I got the currency, I can easily put in the asset and manage it."
+        },
+        user3: {
+            say: "我平时就有投资的习惯，虚拟货币是我很重要投资项目。一直以来我都在币托进行交易，他的加密机制值得我的信赖。"
+        }
+    },
+    faq: {
+        question1: {
+            title: "购买比特币合法吗?",
+            answer: "合法。央行定位为数位虚拟商品，但会员的申请与入会使用会依照实名制与洗钱防制法的相关规定办理。"
+        },
+        question2: {
+            title: "比特币购买一颗的门槛好高?",
+            answer: "在 BitoEX，比特币的最小购买金额是新台币 200 元；在 BitoPro，比特币的最小购买量是 0.0025 颗，约新台币 725 元就能买到比特币。此外，银行帐号汇入款项加值后，必须入金才能购买；反之售出要领回新台币，须要出金，钱才会汇进您绑定的银行帐号。"
+        },
+        question3: {
+            title: "购买比特币的手续费是?",
+            answer: "两个平台的费用标准不一样。BitoEX 手续费包含在即时报价中；BioPro 则采两种费率，市价买进及市价卖出依成交金额收0.2%的手续费，反之挂单成交则依成交金额收 0.1％。"
+        }
+    },
+    footer: {
+        contactUs: "联络我们",
+        press: "媒体报导"
+    }
+  }
+}
